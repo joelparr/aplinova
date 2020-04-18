fbde2c7097f67761011d119842c46feb23327407
Daniel Teixeira da Silva <danieldts2013@gmail.com>
2020-04-18 14:09:33 -0300

    configurando applicacao para heroku

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ const morgan = require('morgan');
 const usuario = require('./routes/usuario.js');
 const path = require('path');
 const Sequelize = require('sequelize');
+//Atributos Prod
+const attributesdb = {
+    username: 'b696fcdf64b8a7',
+    password: '065dcdf8',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    db: 'heroku_a3bd692f7d5af73'
+}
 //DB connection
-const sequelize = new Sequelize('aplinova_dev', 'root', '123456'
-,{
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(attributesdb.db, attributesdb.username, attributesdb.password,{
+        host: attributesdb.host,
+        dialect: 'mysql'
+    });
+
+//DEV
+// const sequelize = new Sequelize('aplinova_dev', 'root', '123456',{
+//         host: 'localhost',
+//         dialect: 'mysql'
+//     });
 
 sequelize.authenticate()
 .then(function(){
@@ -28,4 +40,4 @@ app.use('/public', express.static('public'));
 app.use(morgan('dev'));
 app.use('/', usuario);
 
-app.listen(process.env.APPPORT || 3000);
+app.listen(process.env.PORT || 3000);
