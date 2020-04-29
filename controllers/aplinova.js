609a1f8e1e5c8433a027240de9494d707086f87a
Daniel Teixeira da Silva <danieldts2013@gmail.com>
2020-04-29 20:24:52 -0300

    Desenvolvendo a tela de home

--- controllers/aplinova.js
+++ controllers/aplinova.js
@@ -1,3 +1,7 @@
 exports.show = (req, res)=>{
     res.render('./portal/index');
 }
+
+exports.contato = (req, res)=>{
+    res.render('./portal/contato');
+}
--- routes/aplinova.js
+++ routes/aplinova.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {show} = require('../controllers/aplinova.js');
+const {show, contato} = require('../controllers/aplinova.js');
 //Não preciso estar logado para acessar a plataforma
 //const {isValid} = require('../middlewares/isValidUser.js');
 
 const router = express.Router();
 
 router.get('/', show);
+router.get('/contato', contato);
 
 module.exports = router;
