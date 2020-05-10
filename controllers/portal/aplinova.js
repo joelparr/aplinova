f1f88de01700fdf58b1330688f61b858c011deab
gbgil <gabrielbarbosagil@gmail.com>
2020-05-09 23:05:35 -0300

    APN-23 - Fazer componentes da home - Introdução, cards de produtos, nossa empresa

--- controllers/portal/aplinova.js
+++ controllers/portal/aplinova.js
@@ -4,8 +4,17 @@
  */
 
 //Tela principal do portal
-exports.show = (req, res)=>{
-    res.render('./portal/index');
+exports.show = (req, res) => {
+    const produtos = [
+        {title: 'Aromas', imageUrl: 'https://images.pexels.com/photos/134577/pexels-photo-134577.jpeg', href: 'aromas'},
+        {title: 'Corantes', imageUrl: 'https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg', href: 'corantes'},
+        {title: 'Produtos Veganos e Orgânicos', imageUrl: 'https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg', href: 'produtos-veganos'},
+        {title: 'Proteínas', imageUrl: 'https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg', href: 'proteinas'},
+        {title: 'Substitutos de Açúcar e Fibras', imageUrl: 'https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg', href: 'substitutos'},
+        {title: 'Produtos Naturais', imageUrl: 'https://images.pexels.com/photos/134577/pexels-photo-134577.jpeg', href: 'produtos-naturais'},
+    ]
+
+    res.render('./portal/index', {produtos});
 }
 
 //Tela de contato
