ffacae0159767479c899d536a9958a99102d6018
Daniel <danieldts2013@gmail.com>
2020-05-19 17:48:03 -0300

    deploy heroku teste

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -28,8 +28,8 @@ const {
 //Raiz da chamada acontece no APP.JS
 router.get('/', isValid, index);
 //Get formulario
-router.get('/new/categoria',isValid, newCategoria); //Formulario da categoria - TODO : Ira sair
 router.get('/new/produto', isValid, newProduto); //Formulario do novo produto
+router.get('/new/cat',isValid, newCategoria); //Formulario da categoria - TODO : Ira sair
 router.get('/produto/:id', isValid, showProduto);
 router.get('/subcategoria/:id', isValid, showSubCategoria);
 router.get('/categoria/:id', isValid, showCategoria);
