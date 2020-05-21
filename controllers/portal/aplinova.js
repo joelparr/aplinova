a3f7ddd907ffb787bcce695da092d4c27622452e
gbgil <gabrielbarbosagil@gmail.com>
2020-05-21 02:04:43 -0300

    APN-27 - arrumar os cards de subcategorias

--- controllers/portal/aplinova.js
+++ controllers/portal/aplinova.js
@@ -2,8 +2,7 @@
  * Description: Modulo de controllers de dentro do portal da aplinova
  * Author: Findi
  */
-
- const produtos = [
+const categorias = [
      {title: 'Aromas', imageUrl: 'https://images.pexels.com/photos/134577/pexels-photo-134577.jpeg', href: 'aromas'},
      {title: 'Corantes', imageUrl: 'https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg', href: 'corantes'},
      {title: 'Produtos Veganos e Orgânicos', imageUrl: 'https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg', href: 'produtos-veganos'},
@@ -11,13 +10,48 @@
      {title: 'Substitutos de Açúcar e Fibras', imageUrl: 'https://images.pexels.com/photos/103566/pexels-photo-103566.jpeg', href: 'substitutos'},
      {title: 'Produtos Naturais', imageUrl: 'https://images.pexels.com/photos/134577/pexels-photo-134577.jpeg', href: 'produtos-naturais'},
  ]
+const subcategorias = [
+{
+    titulo: 'Para Panificação',
+    produtos: [
+        'Fermento Biológico Seco (Instant Dry Yeast)',
+        'Melhoradores de Farinhas',
+        'Gordura Vegetal em Pó Encapsulada',
+        'Aromas',
+        'Corantes',
+        'Substitutos de Cacau',
+        'Redutores de Sódio',
+        'Conservantes Naturais',
+        'Mel em Pó, Iogurte em Pó, Doce de Leite em Pó',
+        'Queijos em Pó',
+        'Extrato de Malte',
+        'Emulsificantes'
+    ]
+},
+  {
+    titulo: 'Para Produtos Lácteos',
+    produtos: [
+        'Gordura Vegetal em Pó Encapsulada',
+        'Aromas',
+        'Corantes Naturais',
+        'Conservantes Naturais',
+        'Crispies de Malte'
+    ]
+  },
+  {
+    titulo: 'Para Frutas e Hortaliças',
+    produtos: [
+        'Antioxidante Natural para Extensão de Shelf-Life'
+    ]
+  }
+]
 const models = require('../../models');
 const Categoria = models.Categoria;
 const Produto = models.Produto;
 
 //Tela principal do portal
 exports.show = (req, res) => {
-    res.render('./portal/index', {produtos});
+    res.render('./portal/index', {produtos: categorias});
 }
 
 //Tela de contato
@@ -34,12 +68,12 @@ exports.aromas = (req, res)=>{
     }else{
       res.render('./portal/produtos/corantes', {aromas: undefined});
     }
-    
+
   })
   .catch(function(error){
     res.render('./portal/produtos/aromas', {error});
   })
-  
+
 }
 
 //Tela de corantes
@@ -51,20 +85,20 @@ exports.corantes = (req, res)=>{
     }else{
       res.render('./portal/produtos/corantes', {corantes: undefined});
     }
-    
+
   })
   .catch(function(error){
     res.render('./portal/produtos/corantes', {error});
   })
-  
+
 }
 
 exports.foodservice = (req, res)=>{
-  res.render('./portal/produtos/foodservice', {produtos});
+  res.render('./portal/produtos/foodservice', {subcategorias});
 }
 
 exports.ingredFuncionais = (req, res)=>{
-  res.render('./portal/produtos/ingredientesfuncionais', {produtos});
+  res.render('./portal/produtos/ingredientesfuncionais', {produtos: categorias});
 }
 
 exports.prodNaturais = (req, res)=>{
@@ -79,15 +113,15 @@ exports.prodNaturais = (req, res)=>{
   .catch(function(error){
     res.render('./portal/produtos/produtosnaturais', {error});
   })
-  
+
 }
 
 exports.prodVeganos = (req, res)=>{
-  res.render('./portal/produtos/produtosveganos', {produtos});
+  res.render('./portal/produtos/produtosveganos', {produtos: categorias});
 }
 
 exports.proteinas = (req, res)=>{
-  res.render('./portal/produtos/proteinas', {produtos});
+  res.render('./portal/produtos/proteinas', {produtos: categorias});
 }
 
 exports.revConfeitaria = (req, res)=>{
@@ -102,11 +136,11 @@ exports.revConfeitaria = (req, res)=>{
   .catch(function(error){
     res.render('./portal/produtos/revconfeitaria', {error});
   })
-  
+
 }
 
 exports.subacucar= (req, res)=>{
-  res.render('./portal/produtos/subacucarfibras', {produtos});
+  res.render('./portal/produtos/subacucarfibras', {produtos: categorias});
 }
 
 exports.nossaempresa = (req, res)=>{
