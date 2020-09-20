0267c37501f14f8fe7015d928c5fc64fe2074228
Daniel <danieldts2013@gmail.com>
2020-09-20 16:46:46 -0300

    alteracoes na validacao do formulario de contato

--- public/javascript/portal/contato.js
+++ public/javascript/portal/contato.js
@@ -17,7 +17,6 @@ const textInner = document.getElementById('demo');
 
 //Botao que ira enviar o email para administrador e para o usuario como resposta
 sendEmail.addEventListener('click', function(event){
-    console.log(event);
     event.preventDefault();
     //Validacao do formulario
     if(nome.value === "" || email.value === "" || telefone.value === "" || empresa.value === "" || mensagem.value === ""){
@@ -26,6 +25,12 @@ sendEmail.addEventListener('click', function(event){
         return 0;
     }
 
+    //Validando as infos do formulario
+    if(!validacaoFormulario()){
+        return 0;
+    }
+
+    //criando os dados que sera enviados
     const data = {
         nome: nome.value,
         email: email.value,
@@ -40,6 +45,8 @@ sendEmail.addEventListener('click', function(event){
         body: JSON.stringify(data)
     };
 
+    enviandoMensagem();
+
     fetch("/sendContato", options)
     .then(result=>{
         return result.json();
@@ -52,14 +59,88 @@ sendEmail.addEventListener('click', function(event){
             telefone.value = "";
             empresa.value = "";
             mensagem.value = "";
+            document.getElementById("demo").classList.remove('blinking');
             document.getElementById("demo").innerHTML = "Seu email foi enviado com sucesso";
             textInner.style.color = "lightgray";
+            habilitaEntradas();
         }else{
+            document.getElementById("demo").classList.remove('blinking');
             document.getElementById("demo").innerHTML = "Houve um problema ao enviar o email";
             textInner.style.color = "red";
+            habilitaEntradas();
         }
     })
     .catch(err=>{
         console.log(err);
+        document.getElementById("demo").classList.remove('blinking');
+        document.getElementById("demo").innerHTML = "Houve um problema ao enviar o email";
+        textInner.style.color = "red";
+        habilitaEntradas();
     })
 });
+
+function validacaoFormulario(){
+    limpaBorder(); //limpando as borders;
+    let msg = "";
+    if((nome.value).length < 2){
+        msg = "O nome precisa ter mais caracteres para ser válido.";
+        nome.style.borderBottom = '1px solid red';
+    }else if(!(/([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+/.test(email.value))){
+        msg = "O email precisa ser válido.";
+        email.style.borderBottom = '1px solid red';
+    }else if((telefone.value).length > 14 || (telefone.value).length < 10){
+        msg = "O telefone esta com numero de caracteres errado.";
+        telefone.style.borderBottom = '1px solid red';
+    }else if(/([a-zA-Z])/.test(telefone.value)){
+        msg = "O telefone nao pode conter letras.";
+        telefone.style.borderBottom = '1px solid red';
+    }else if(!(/(\(*\d{2}\)*)\s*(\d{4,5})\-*(\d{4,6})/.test(telefone.value))){
+        msg = "O telefone precisa ter um padrao de (area) nnnnnnnnn";
+        telefone.style.borderBottom = '1px solid red';
+    }else if((empresa.value).length < 2){
+        msg = "A empresa precisa ter mais caracteres.";
+        empresa.style.borderBottom = '1px solid red';
+    }else if((mensagem.value).length < 5){
+        msg = "A mensagem esta com poucos caracteres";
+        mensagem.style.borderBottom = '1px solid red';
+    }
+
+    if(msg !== "") document.getElementById("demo").innerHTML = msg;
+    if(msg !== "") textInner.style.color = "red";
+    if(msg !== "") return false;
+    return true;
+}
+
+function enviandoMensagem(){
+    desabilitaEntradas()
+    let msg = "Enviando email...";
+    document.getElementById("demo").innerHTML = msg;
+    document.getElementById("demo").classList.add('blinking');
+}
+
+function limpaBorder(){
+    email.style.borderBottom = '1px solid #ececec';
+    nome.style.borderBottom = '1px solid #ececec';
+    empresa.style.borderBottom = '1px solid #ececec';
+    mensagem.style.borderBottom = '1px solid #ececec';
+    telefone.style.borderBottom = '1px solid #ececec';
+    document.getElementById("demo").innerHTML = "";
+}
+
+function desabilitaEntradas(){
+    sendEmail.disabled = true;
+    nome.readOnly = true;
+    email.readOnly = true;
+    empresa.readOnly = true;
+    mensagem.readOnly = true;
+    telefone.readOnly = true;
+}
+
+function habilitaEntradas(){
+    sendEmail.disabled = false;
+    nome.readOnly = false;
+    email.readOnly = false;
+    empresa.readOnly = false;
+    mensagem.readOnly = false;
+    telefone.readOnly = false;
+}
