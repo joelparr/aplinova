e9946f251afa303aa58571ae39d3985f7685cceb
Daniel <danieldts2013@gmail.com>
2020-10-19 21:20:35 -0300

    criado o middleware para redirecionamento de http apra https

---
+++ middlewares/httpToHttps.js
@@ -0,0 +1,12 @@
+exports.httpToHttps = (req, res, next)=>{
+    const local = req.url;
+    const schema = (req.headers['x-forwarded-proto'] || "").toLowerCase();
+    const www = req.headers.host
+    const fullUrl = `https://${www}${local}`;
+    
+    const notLocalHost = ()=> www.indexOf('localhost') < 0;
+    if(notLocalHost() && schema !== 'https'){
+        res.redirect(fullUrl);
+    }
+    next();
+}
