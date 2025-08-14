d7d328b8ce8557137b589a7bbd9d690257792632
Joel Parr <joel.parr@gmail.com>
2025-08-14 11:33:40 +0100

    Updated mail config

--- config/mail.js
+++ config/mail.js
@@ -1,14 +1,25 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-const config={
-    host: process.env.MAILHOST,
-    port: process.env.MAILPORT,
-    secure: false,
-    auth:{
-        user: process.env.MAILUSER,
-        pass: process.env.MAILPASS
-    }
-}
+// const config={
+//     host: process.env.MAILHOST,
+//     port: process.env.MAILPORT,
+//     secure: false,
+//     auth:{
+//         user: process.env.MAILUSER,
+//         pass: process.env.MAILPASS
+//     }
+// }
 
-module.exports = nodemailer.createTransport(config);
+const transportConfig = {
+    host: process.env.MAILHOST,
+    port: Number(process.env.MAILPORT),
+    secure: String(process.env.MAILPORT) === '465',
+    auth: { user: process.env.MAILUSER, pass: process.env.MAILPASS }
+};
+
+const defaultOptions = {
+  from: process.env.MAILFROM || 'Aplinova <no-reply@example.com>'
+};
+
+module.exports = nodemailer.createTransport(transportConfig, defaultOptions);
